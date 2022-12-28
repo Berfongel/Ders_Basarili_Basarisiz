6a873502371299ef1d964a93d000185bce7b8945
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 16:09:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sayi1;
+Console.WriteLine("Dersten aldığınız notunu giriniz : ");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+if (sayi1 >= 50)
+{
+    Console.WriteLine("Başarılı ");
+}
+else 
+{
+    Console.WriteLine("Başarısız ");
+}
+
